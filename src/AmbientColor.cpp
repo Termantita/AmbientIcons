18d8e0f97c021698f5f325b8f312ebb0783fa927
Terma <termafaxplay@outlook.com>
2024-03-28 16:48:24 -0300

    Just realized i have m_player1 & m_player2 members for easy access

--- src/AmbientColor.cpp
+++ src/AmbientColor.cpp
@@ -31,7 +31,7 @@ ccColor3B AmbientColor::getRenderColor(CCSprite *bgSprite) {
 
 ccColor3B AmbientColor::getScreenColor() {
 	if (!(m_changeMainColor || m_changeSecondaryColor || m_changeMainColorDual ||
-			m_changeSecondaryColorDual || m_changeWaveTrail || m_changeGlowColor))
+		m_changeSecondaryColorDual || m_changeWaveTrail || m_changeGlowColor))
 		return ccColor3B(0.f, 0.f, 0.f);
 
 	m_pickPos = CCPoint(getRenderXPos(), getRenderYPos());
@@ -41,9 +41,8 @@ ccColor3B AmbientColor::getScreenColor() {
 	auto oldPos = m_layer->getPosition();
 
 	m_layer->setPositionX(-size.width * m_pickPos.x);
-	if (m_playerFollowPicker && !m_BGColorPicker) { // Follow player picker
-		auto player = m_layer->m_player1;
-		auto playerPos = player->getRealPosition();
+	if (m_playerFollowPicker && m_player2->getPositionX() == 0 && m_player2->getPositionY() == 0) { // Follow player picker
+		auto playerPos = m_player1->getRealPosition();
 		auto playerPosOffset = playerPos.y * (getPlayerFollowOffset() + 1.f);
 		auto screenPlayerPosY = playerPosOffset / size.height < 1.f ? playerPosOffset / size.height : 1.f; // Convert player level pos to player screen pos 0-1.f (related to level -> related to screen)
 
@@ -95,10 +94,10 @@ void AmbientColor::setIconColor(ccColor3B color) {
 
 	if (m_changeWaveTrail) {
 		if (m_player1->m_isDart) {
-		m_player1->m_waveTrail->setColor(color);
+			m_player1->m_waveTrail->setColor(color);
 		}
 		if (m_player2->m_isDart) {
-		m_player2->m_waveTrail->setColor(color);
+			m_player2->m_waveTrail->setColor(color);
 		}
 	}
 
