233faa7c79ba52271eba4d88dc9a0f0dae5ba350
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-03-06 21:21:06 +0100

    refactor settings, improve performance, and more

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
@@ -21,31 +22,66 @@ public:
 		NONE
 	};
 
-	static ColorPicker getPicker() {
-		if (Mod::get()->getSettingValue<std::string>("color-picker") == "Background")
-			return ColorPicker::BG;
-		return ColorPicker::SCREEN;
-	}
+	static inline bool enabled;
+	static inline PlayerColor player1Color;
+	static inline PlayerColor player2Color;
+	static inline Extra player1Extra;
+	static inline Extra player2Extra;
+	static inline ColorPicker colorPicker;
+	static inline bool changeMethodWhenBlack;
+	static inline int updateTime;
+	static inline CCPoint renderPosition;
+	static inline bool drawRenderPosition;
+	static inline bool debugColor;
 
-	static Extra getExtra(bool isP2 = false) {
-		auto setting = isP2 ? "change-p2-extra" : "change-p1-extra";
-		auto option = Mod::get()->getSettingValue<std::string>(setting);
-		
-		if (option == "Wave Trail")
-			return Extra::WAVE_TRAIL;
-		if (option == "None")
-			return Extra::NONE;
-		return Extra::GLOW;
-	}
+	static void updateSettings() {
+		enabled = Mod::get()->getSettingValue<bool>("mod-enabled");
 
-	static PlayerColor getPlayerPreference(bool isP2 = false) {
-		auto setting = isP2 ? "change-p2-color" : "change-p1-color";
-		auto preference = Mod::get()->getSettingValue<std::string>(setting);
+		auto parseChangeColor = [](const std::string& key) {
+			auto value = Mod::get()->getSettingValue<std::string>(key);
+			if (value == "Main") {
+				return MAIN;
+			} else if (value == "Secondary") {
+				return SECONDARY;
+			} else if (value == "Both") {
+				return BOTH;
+			}
 
-		if (preference == "Main")
-			return PlayerColor::MAIN;
-		if (preference == "Secondary")
-			return PlayerColor::SECONDARY;
-		return PlayerColor::BOTH;
+			return MAIN; // shouldn't happen
+		};
+
+		auto parseChangeExtra = [](const std::string& key) {
+			auto value = Mod::get()->getSettingValue<std::string>(key);
+			if (value == "None") {
+				return NONE;
+			} else if (value == "Wave Trail") {
+				return WAVE_TRAIL;
+			} else {
+				return GLOW;
+			}
+		};
+
+		player1Color = parseChangeColor("change-p1-color");
+		player2Color = parseChangeColor("change-p2-color");
+		player1Extra = parseChangeExtra("change-p1-extra");
+		player2Extra = parseChangeExtra("change-p2-extra");
+
+		auto colorp = Mod::get()->getSettingValue<std::string>("color-picker");
+		if (colorp == "Background") {
+			colorPicker = BG;
+		} else {
+			colorPicker = SCREEN;
+		}
+
+		changeMethodWhenBlack = Mod::get()->getSettingValue<bool>("on-black:change-to-screen-picker");
+		updateTime = Mod::get()->getSettingValue<int64_t>("update-time");
+
+		renderPosition = CCPoint{
+			static_cast<float>(Mod::get()->getSettingValue<double>("render-x-pos")),
+			static_cast<float>(Mod::get()->getSettingValue<double>("render-y-pos"))
+		};
+
+		drawRenderPosition = Mod::get()->getSettingValue<bool>("draw-pos");
+		debugColor = Mod::get()->getSettingValue<bool>("debug-color");
 	}
 };
